f8944462d000de73c15ad926c076259f3ace7402
ryanjayretutar <suarezjobelle>
2019-01-08 16:42:20 +0800

    Tuesday

--- assets/js/dashboardfunction.js
+++ assets/js/dashboardfunction.js
@@ -20,3 +20,41 @@
 //  $('[data-spy="scroll"]').each(function () {
 //   var $spy = $(this).scrollspy('refresh')
 // })
+
+//vertical nav
+
+$(document).ready(function() {
+
+  $("#tab1_content").hide();
+  $("#tab2_content").hide();
+  $("#tab3_content").hide();
+  $("#tab4_content").hide();
+
+  $("#tab1").click(function() {
+    $("#tab1_content").show();
+    $("#tab2_content").hide();
+    $("#tab3_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab2").click(function() {
+    $("#tab2_content").show();
+    $("#tab1_content").hide();
+    $("#tab3_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab3").click(function() {
+    $("#tab3_content").show();
+    $("#tab1_content").hide();
+    $("#tab2_content").hide();
+    $("#tab4_content").hide();
+  });
+
+  $("#tab4").click(function() {
+    $("#tab4_content").show();
+    $("#tab1_content").hide();
+    $("#tab2_content").hide();
+    $("#tab3_content").hide();
+  });
+});
